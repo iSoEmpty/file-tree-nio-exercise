8db493d42764f4409f7d8cf7cd83bac8aeb8719b
Sofia Krylova <sofia_krylova@epam.com>
2022-08-23 16:00:26 +0200

    Initial

---
+++ src/main/java/com/epam/autotasks/FileTree.java
@@ -0,0 +1,11 @@
+package com.epam.autotasks;
+
+import java.nio.file.Path;
+import java.util.Optional;
+
+public class FileTree {
+
+    public Optional<String> tree(final Path path) {
+        return null;
+    }
+}
